a812cbcec66e1eb9f1b302f7575f2795a15da248
Joshua Zenn <wordman05@gmail.com>
2020-11-13 19:24:50 -0500

    Basic reboot functionality and loading from YAML

--- api/views.py
+++ api/views.py
@@ -5,9 +5,18 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from proxmoxer import ProxmoxAPI
+import yaml
+
+stream = open('proxmox.yml', 'r')
+px_vars = yaml.full_load(stream)
+proxmox = ProxmoxAPI(px_vars['proxmox_host'], user=px_vars['proxmox_user'], password=px_vars['proxmox_password'], verify_ssl=px_vars['proxmox_verify_ssl'])
 
 
 @api_view(["GET"])
-def welcome(request):
-    content = {"message": "Welcome to the BookStore!"}
-    return JsonResponse(content)
+def trigger(request):
+    hostname = request.GET['host']
+    for vm in proxmox.cluster.resources.get(type='vm'):
+        if vm['name'] == hostname:
+            return JsonResponse(proxmox.nodes(vm['node']).lxc(vm['vmid']).status.reboot.post(), safe=False)
+    return JsonResponse("An error occured", safe=False)
