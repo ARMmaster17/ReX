2e89f7c75f178663da3af88e8d2c8a206f764f3a
Joshua Zenn <wordman05@gmail.com>
2020-11-14 22:44:49 -0500

    Added support for Vigil webhooks
    Closes #9

--- api/views.py
+++ api/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from urllib.parse import urlparse
+
 from django.shortcuts import render
 
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework.utils import json
+
 from api.actions import perform_actions
 
 
@@ -13,4 +17,15 @@ def trigger(request):
     hostname = request.GET['host']
     # TODO: Verify that hostname is not empty/invalid.
     perform_actions(hostname)
+    return JsonResponse("OK", safe=False)
+
+
+@api_view(["POST"])
+def vigil(request):
+    json_payload = json.loads(request.body)
+    for replica in json_payload['replicas']:
+        if not json_payload['status'] == "dead":
+            continue
+        domain = urlparse(replica).netloc
+        perform_actions(domain)
     return JsonResponse("OK", safe=False)
